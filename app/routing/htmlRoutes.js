6021a2702ff1e33b6de5d5a2d25335efa434aa11
Handrew <andrew.a.han@gmail.com>
2018-05-05 22:53:04 -0400

    created routing files that connects to the server.js

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var Routes = express.Router();
+
+// going to need express application in each of the files that have the routes. We are going to assign it to module.exports so that all other files can access this 'app' variable/object. 
+// var app = module.exports = express();
+
+//Routes
+Routes.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, './app/public/home.html'));
+});
+
+Routes.get('/survey', function(req, res) {
+    res.sendFile(path.join(__dirname, './app/public/survey.html'));
+});
+
+module.exports = Routes;
--- server.js
+++ server.js
@@ -18,37 +18,11 @@ app.use(bodyParser.json());
 //friends array to add all the datas
 var friends = [];
 
-//Routes
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, 'home.html'));
-});
+var router = require('./app/routing/htmlRoutes.js')
+app.use(router);
 
-app.get('/survey', function(req, res) {
-    res.sendFile(path.join(__dirname, 'survey.html'));
-});
-
-//routes for looking at the apis
-app.get('/api/friendsList/:buddy', function( req, res) {
-    var chosen = req.params.buddy;
-
-    console.log(chosen);
-
-// using a method in javascript for arrays called find() to check for a specific friend who had the same choices.  Going to use a boolean value to deterine if a specific element in the array matches.
-    const friend = friends.find(f => f.name === chosen);
-    if(!friend) res.status(404).send("Nothing found");
-    res.send(friend);
-});
-
-//route that will post information given through the users to this api
-app.post('/api/friendsList', function(req, res) {
-    var newFriend = req.body;
-
-    console.log(newFriend);
-
-    friends.push(newFriend);
-
-    res.json(newFriend);
-})
+var Routes = require('./app/routing/apiRoutes.js');
+app.use(Routes);
 
 //======================================================
 
