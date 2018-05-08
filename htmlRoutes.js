84374979e476ac2f41c0a2a51010cb93a0ede0b3
Handrew <andrew.a.han@gmail.com>
2018-05-07 22:48:10 -0400

    formatted all the files

--- htmlRoutes.js
+++ htmlRoutes.js
@@ -5,11 +5,11 @@ var path = require('path');
 
 //Routes
 
-Routes.get('/', function(req, res) {
+Routes.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, './app/public/home.html'));
 });
 
-Routes.get('/survey', function(req, res) {
+Routes.get('/survey', function (req, res) {
     res.sendFile(path.join(__dirname, './app/public/survey.html'));
 });
 
