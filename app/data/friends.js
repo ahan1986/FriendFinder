e722aa8e5eb693bb93c3b48ebcf0e7f40ed5c735
Handrew <andrew.a.han@gmail.com>
2018-05-07 22:46:40 -0400

    created more friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,19 +2,51 @@
 
 var friends = [
     {
-        'name': 'Andrew',
+        'name': 'RickJames!',
+        'photo': "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
+        'scores': [
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1
+        ]
+    },
+    {
+        'name': 'BobDoleSexMachine!',
         'photo': "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
         'scores': [
             3,
-            4,
-            5,
-            1,
             3,
             3,
-            2,
-            5,
-            4,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
             3
+        ]
+    },
+    {
+        'name': 'BillC&MonicaL',
+        'photo': "http://i0.kym-cdn.com/entries/icons/original/000/012/812/Catbug_by_sircinnamon-d5riz9k.png",
+        'scores': [
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5
         ]
     }
 ]
